1b1d224d1b1bd986399e1eafdfeffe1219cf1ff8
aindrathin <aindrathin@hotmail.com>
2018-02-05 13:48:21 -0800

    Initial Commit

---
+++ iKid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Aindra Thin on 2/5/18.
+//  Copyright © 2018 Aindra Thin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
